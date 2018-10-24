eafa7b381b35ffedb2d3b6484ce3d5bf2744c2b4
yisen614 <yisen614@gmail.com>
2018-10-24 22:37:45 +0800

    update

--- src/main/java/top/yisen614/client/ClientApplication.java
+++ src/main/java/top/yisen614/client/ClientApplication.java
@@ -7,8 +7,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 @EnableDiscoveryClient
 public class ClientApplication {
-
-//	emergency! eureka may be incorrectly claiming instances are up when they're not. renewals are lesser than threshold and hence the instances are not being expired just to be safe.
 	public static void main(String[] args) {
 		SpringApplication.run(ClientApplication.class, args);
 	}
